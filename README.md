414b58603b95ff4053370bbaecffae9a716a29df
= <=>
2024-07-01 17:14:10 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# veri_mi_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/veri_mi_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.veri_mi_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
